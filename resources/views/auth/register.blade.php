f9c5436e8b5c70ad6f7190e7f5327a6cd74ba001
n.shiraishi <20170929friday@gmail.com>
2020-04-20 08:22:45 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-sm-8 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                アカウント作成
+            </div>
+            
+            <div class="card-body text-center">
+                <div class="col-sm-9 offset-sm-3 mx-auto">
+                    {!! Form::open(['route' => 'signup.post']) !!}
+                        <div class="form-group">
+                            {!! Form::label('name', 'ユーザ名') !!}
+                            {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                        </div>
+                        
+                        <div class="form-group">
+                            {!! Form::label('email', 'メールアドレス') !!}
+                            {!! Form::email('email',old('email'), ['class' => 'form-control']) !!}
+                        </div>
+                        
+                        <div class="form-group">
+                            {!! Form::label('password', 'パスワード') !!}
+                            {!! Form::password('password', ['class' => 'form-control']) !!}
+                        </div>
+                        
+                        <div class="form-group">
+                            {!! Form::label('password_confirmation', 'パスワード（確認）') !!}
+                            {!! Form::password('password', ['class' => 'form-control']) !!}
+                        </div>
+                        
+                        {!! Form::submit('登録する', ['class' => 'btn btn-primary btn-block']) !!}
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
